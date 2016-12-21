d6a0907347af8eeae28ff9e1f44a8d92dfec59ca
Alexey Zvegintcev <alexey.zvegintcev@sysgears.com>
2016-12-21 15:10:20 +0200

    Added travis scripts

---
+++ travis-build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "### Running tests"
+
+cd script && ./jenkins-build-unit
+cd ../grails/ag-plugin && ./gradlew test
+
+echo "### Running publishing"
+
+if [[ $TRAVIS_BRANCH == 'grails3' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
+	echo "### publishing plugin to bintray"
+	#cd ../dao-plugin && ./gradlew bintrayUpload
+
+else
+  echo "TRAVIS_BRANCH: $TRAVIS_BRANCH"
+  echo "TRAVIS_REPO_SLUG: $TRAVIS_REPO_SLUG"
+  echo "TRAVIS_PULL_REQUEST: $TRAVIS_PULL_REQUEST"
+fi
